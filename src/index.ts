46b56f2df439e86afef40987d34248ecbfc6c3c3
Aman <Sharmaamqn@gmail.com>
2026-02-04 22:58:09 +0530

    set up get subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import subjectsRouter from "./routes/subjects";
 
 const app = express();
 const PORT = 3000;
 
+if (process.env.FRONTEND_URL) {
+  throw new Error("FRONTEND URL not found in .env");
+}
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
+
 // Middleware
 app.use(express.json());
+
+app.use("/api/subjects", subjectsRouter);
 
 // Routes
 app.get("/", (req, res) => {
